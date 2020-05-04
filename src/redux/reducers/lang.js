1fff85cb09c00e83a02fb0cbcb1eee8cbf981766
danieluy <danielsosa.uy@gmail.com>
2020-05-04 18:14:34 -0300

    Added lang toggle button

--- src/redux/reducers/lang.js
+++ src/redux/reducers/lang.js
@@ -2,27 +2,50 @@ import { TOGGLE_APP_LANG } from '../actions';
 import LANG, { getDefaultLang } from '../../lang';
 
 const initialState = () => {
-  const lang = getDefaultLang();
-  return {
-    selected: lang,
-    ...LANG[lang],
-  };
+  try {
+    let lang = getDefaultLang();
+    const preferred = window.localStorage.getItem('PREFERRED_LANGUAGE');
+    if (preferred) {
+      lang = preferred;
+    }
+    return {
+      selected: lang,
+      ...LANG[lang],
+    };
+  }
+  catch (err) {
+    console.error(err);
+    return {
+      selected: 'ES',
+      ...LANG.ES,
+    };
+  }
 };
 
 export default (state = initialState(), action) => {
   switch (action.type) {
-    case TOGGLE_APP_LANG: {
-      if (state.selected === 'ES') {
+    case TOGGLE_APP_LANG:
+      try {
+        if (state.selected === 'ES') {
+          window.localStorage.setItem('PREFERRED_LANGUAGE', 'EN');
+          return {
+            selected: 'EN',
+            ...LANG.EN,
+          };
+        }
+        window.localStorage.setItem('PREFERRED_LANGUAGE', 'ES');
+        return {
+          selected: 'ES',
+          ...LANG.ES,
+        };
+      }
+      catch (err) {
+        console.error(err);
         return {
           selected: 'EN',
           ...LANG.EN,
         };
       }
-      return {
-        selected: 'ES',
-        ...LANG.ES,
-      };
-    }
     default:
       return state;
   }

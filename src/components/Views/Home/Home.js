7d454ec2e1d30b28963cb146a113bc663cf11109
danieluy <danielsosa.uy@gmail.com>
2020-05-29 18:01:40 -0300

    Saving progress

--- src/components/Views/Home/Home.js
+++ src/components/Views/Home/Home.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// Material UI
-import TYpography from '@material-ui/core/Typography';
+import useStyles from './styles';
+import { useSelector } from 'react-redux';
+import { selectHomeLang } from '../../../redux/selectors';
+// Components
+import Article from '../../Article';
 
 function Home() {
+  const classes = useStyles();
+  const lang = useSelector(selectHomeLang);
+  const {
+    title,
+    articles: { whatWhy, accessibility, design, performance, tech },
+  } = lang;
+
+  document.title = `Daniel Sosa | ${title}`;
+
   return (
-    <TYpography>
-      {'<Home />'}
-    </TYpography>
+    <section className={classes.section}>
+      <Article articleId="what-why" title={whatWhy.title} banner={whatWhy.banner} paragraphs={whatWhy.paragraphs} />
+      <Article articleId="accessibility" title={accessibility.title} banner={accessibility.banner} paragraphs={accessibility.paragraphs} />
+      <Article articleId="design" title={design.title} banner={design.banner} paragraphs={design.paragraphs} />
+      <Article articleId="performance" title={performance.title} banner={performance.banner} paragraphs={performance.paragraphs} />
+      <Article articleId="tech" title={tech.title} banner={tech.banner} paragraphs={tech.paragraphs} />
+    </section>
   );
 }
 

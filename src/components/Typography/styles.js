4fc77fefafabd0d69d2b99f61c3b57f46601438c
danieluy <danielsosa.uy@gmail.com>
2020-06-03 14:33:27 -0300

    Added onFocus styles to titles

--- src/components/Typography/styles.js
+++ src/components/Typography/styles.js
@@ -6,6 +6,7 @@ export default makeStyles(theme => {
       marginBottom: theme.spacing(2),
       outline: 'none',
       fontSize: theme.spacing(4),
+      ...onFocus(theme.spacing(4)),
     },
     title2: {
       marginBottom: theme.spacing(),
@@ -28,4 +29,33 @@ export default makeStyles(theme => {
       fontSize: theme.spacing(2),
     },
   };
+
+  function onFocus(fontSize) {
+    const border = fontSize / 32; // half of the triangle bullet side
+    const heightSideRatio = 0.866; // (√3 * side) / 2;
+    const borderLeft = heightSideRatio * 2 * border;
+    return {
+      position: 'relative',
+      // paddingLeft: theme.spacing(4),
+      fontWeight: theme.typography.fontWeightLight,
+      outline: 'none',
+      transition: 'transform 300ms ease-in-out',
+      '&:focus': {
+        transform: `translate(${theme.spacing(3)}px)`,
+        '&::after': {
+          position: 'absolute',
+          content: '""',
+          left: -1 * theme.spacing(3),
+          top: theme.spacing(2.5),
+          width: 0,
+          height: 0,
+          display: 'inline-block',
+          borderTop: `${theme.spacing(border)}px solid transparent`,
+          borderBottom: `${theme.spacing(border)}px solid transparent`,
+          borderLeft: `${theme.spacing(borderLeft)}px solid ${theme.palette.text.primary}`,
+          borderRight: `${theme.spacing(border)}px solid transparent`,
+        },
+      },
+    };
+  }
 });
